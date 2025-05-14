dd5ae47add263d2b21ec732b37ba38fd986d1025
Happy-Franck <solofonirina35@gmail.com>
2025-05-15 00:01:44 +0300

    Migrations ok

---
+++ database/migrations/2025_05_14_205041_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produits', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom');
+            $table->integer('quantite');
+            $table->string('marque');
+            $table->decimal('prix', 10, 2);
+            $table->string('couleur')->nullable();
+            $table->text('config')->nullable();
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->foreignId('categorie_id')->constrained('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
